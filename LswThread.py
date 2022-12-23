60a47ffb4ff11473b598d9ac648136253c52f8d5
Ductory <111000413+Ductory@users.noreply.github.com>
2022-12-23 20:02:17 +0800

    Add files via upload

--- LswThread.py
+++ LswThread.py
@@ -28,14 +28,15 @@ def thread(uid):
 			response = requests.get(url).json()
 			for i in response['post_list']:
 				content = i['content']
-				for t in i['first_post_content']:
-					tmp = t['type'] #对表情、图片、视频特殊处理
-					if tmp == 2: #smiley
-						content = re.sub(pSmiley, 'src=', content, 1)
-					elif tmp == 3: #image
-						content = re.sub(pImage, f'src="{t["origin_src"]}">', content, 1)
-					elif tmp == 5: #video
-						content = re.sub(pVideo, f'<a target= "_blank" href="{t["link"]}"><img src="{t["src"]}"></a>', content, 1)
+				if 'first_post_content' in i:
+					for t in i['first_post_content']:
+						tmp = t['type'] #对表情、图片、视频特殊处理
+						if tmp == 2: #smiley
+							content = re.sub(pSmiley, 'src=', content, 1)
+						elif tmp == 3: #image
+							content = re.sub(pImage, f'src="{t["origin_src"]}">', content, 1)
+						elif tmp == 5: #video
+							content = re.sub(pVideo, f'<a target= "_blank" href="{t["link"]}"><img src="{t["src"]}"></a>', content, 1)
 				f.write(f"<li>在<a target='_blank' href='https://tieba.baidu.com/f?kw={i['forum_name']}'>{i['forum_name']}</a>吧发帖：<a target='_blank' href='https://tieba.baidu.com/p/{i['thread_id']}'>{i['title']}</a>&emsp;<span>{time.strftime('%Y年%m月%d日 %H时%M分%S秒', time.localtime(int(i['create_time'])))}</span><p>{content}</p><hr>\n")
 		f.write('</ol>')
 	return
@@ -58,5 +59,6 @@ def sign(src):
 	s += 'tiebaclient!!!'
 	return hashlib.md5(s.encode('utf-8')).hexdigest()
 
-
+#264432381 雷绍武的uid
+#5530535828 最爱春雷2021的uid
 thread('5530535828') #获取最爱春雷2021的全部帖子
